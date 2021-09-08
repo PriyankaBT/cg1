c65232fd79e816da8ca5e77caf7d2f7a1d579647
PriyankaBT <priyankamurkute25@gmail.com>
2021-09-08 16:37:37 +0530

    try

---
+++ src/DemoLog4J.java
@@ -0,0 +1,24 @@
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.Priority;
+import org.apache.log4j.PropertyConfigurator;
+public class DemoLog4J {
+	public static void main(String[] args) {
+		Logger myLogger = Logger.getLogger(DemoLog4J.class.getName());
+		PropertyConfigurator.configure("resources/log4j.properties");
+		int a = -1;
+		boolean flag = false;
+		myLogger.setLevel((Level) Priority.DEBUG);
+		
+		myLogger.info("Logging information for DemoLog4j");
+		myLogger.debug("Code debugging started");
+		if (a < 0) {
+			myLogger.warn("This is last time warning");
+		}
+
+		if (flag == false) {
+			myLogger.error("Error occured due to false value");
+		}
+	}
+
+}
